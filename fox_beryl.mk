a52be4c993caa5a62e6935a5d1bb095f58257602
sairajkandkar007 <sairajkandkar0110@gmail.com>
2026-09-07 17:59:47 +0530

    Add brand, model, and manufacturer to fox_beryl.mk

--- fox_beryl.mk
+++ fox_beryl.mk
@@ -2,6 +2,7 @@ $(call inherit-product, device/xiaomi/beryl/device.mk)
 
 PRODUCT_DEVICE := beryl
 PRODUCT_NAME := fox_beryl
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO M7 Pro 5G
 PRODUCT_MANUFACTURER := Xiaomi
