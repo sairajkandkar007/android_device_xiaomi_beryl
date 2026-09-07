d6ef3a20b795cdefc32dc1e737f24f616f437a2b
sairajkandkar007 <sairajkandkar0110@gmail.com>
2026-09-07 11:18:22 +0530

    Update fox_beryl.mk with device properties

--- fox_beryl.mk
+++ fox_beryl.mk
@@ -1,6 +1,8 @@
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fox_beryl.mk
+$(call inherit-product, device/xiaomi/beryl/device.mk)
 
-COMMON_LUNCH_CHOICES := \
-    fox_beryl-eng
+PRODUCT_DEVICE := beryl
+PRODUCT_NAME := fox_beryl
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO M7 Pro 5G
+PRODUCT_MANUFACTURER := Xiaomi
