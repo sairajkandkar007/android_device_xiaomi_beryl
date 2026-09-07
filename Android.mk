e702b517a22888d16b99dd82fc64f0feeae25a31
sairajkandkar007 <sairajkandkar0110@gmail.com>
2026-09-07 18:12:03 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,14 +1,3 @@
 LOCAL_PATH := $(call my-dir)
 
-# Device-tree build definitions are provided through the
-# standard OrangeFox/AOSP recovery build system.
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := beryl_device_tree
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := README.md
-LOCAL_MODULE_PATH := $(TARGET_OUT_RECOVERY)
-
-include $(BUILD_PREBUILT)
+# Device tree contains no standalone build modules.
