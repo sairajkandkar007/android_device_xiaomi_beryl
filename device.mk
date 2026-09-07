964fe645204a4a7a11ae66440e0e573d66dcf6c4
sairajkandkar007 <sairajkandkar0110@gmail.com>
2026-09-07 18:14:16 +0530

    Modify device.mk for USB config and remove A/B packages
    Removed A/B packages from device.mk and updated USB configuration comments.

--- device.mk
+++ device.mk
@@ -7,16 +7,11 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO M7 Pro 5G
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Device tree namespace
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# A/B
-PRODUCT_PACKAGES += \
-    fastbootd
 
 # Stock device properties
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -33,7 +28,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.product.page_size=4096 \
     ro.minui.pixel_format=BGRA_8888
 
-# Recovery USB configuration observed on stock beryl.
+# Stock recovery USB configuration
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.recovery.usb.vid=18D1 \
     ro.recovery.usb.adb.pid=D001 \
