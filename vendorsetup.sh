97621abc7cba06d23cd1ec4eff5cd202f0b3b22d
sairajkandkar007 <sairajkandkar0110@gmail.com>
2026-09-10 14:33:36 +0530

    Update vendorsetup.sh for POCO M7 Pro 5G

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# ============================================================
+# POCO M7 Pro 5G (beryl)
+# OrangeFox 14.1
+# Android 16 / API 36
+# ============================================================
+
 export FOX_BUILD_DEVICE=beryl
 
 # A/B
@@ -8,7 +16,10 @@ export FOX_VIRTUAL_AB_DEVICE=1
 export FOX_VENDOR_BOOT_RECOVERY=1
 export FOX_INSTALLER_VENDOR_BOOT_RAMDISK_INSTALL=1
 
-# Vanilla Android recovery
+# Android 16 / API 36
+export FOX_ADD_API_V36_PREBUILTS=1
+
+# Vanilla recovery
 export FOX_VANILLA_BUILD=1
 
 # OrangeFox utilities
@@ -17,18 +28,5 @@ export FOX_USE_NANO_EDITOR=1
 export FOX_USE_TAR_BINARY=1
 export FOX_USE_LZ4_BINARY=1
 
-# Android 16 / API 36
-export FOX_ADD_API_V36_PREBUILTS=1
-
 # AIDL boot control
 export OF_USE_AIDL_BOOT_CONTROL=1
-
-# Do not enable until an exact stock vendor_boot reference
-# has been added and validated.
-# export FOX_REFERENCE_VENDOR_BOOT_IMAGE=$(gettop)/device/xiaomi/beryl/prebuilt/vendor_boot.img
-
-# Enable only after FBE testing demonstrates that it is required.
-# export OF_FORCE_USE_RECOVERY_FSTAB=1
-
-# Experimental Virtual A/B behavior.
-# export OF_VAB_ORS_WIPE_DATA_IS_FORMAT=1
